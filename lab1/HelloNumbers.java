d6e07043bc06f2db8a78cb99b0336e3a37e797b7
Djokovic1999 <zhangcharlie@126.com>
2020-05-11 15:55:49 -0400

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+
+public class HelloNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int x = 0;
+			int sum = 0;
+			while(x < 10) {
+				sum = sum + x;
+				x = x + 1;
+				System.out.println(sum + " ");
+			}
+	}
+
+}
