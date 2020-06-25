f487f545ee3b083dfff68a4b6bd8ab78f70e8043
CharlieZhang1999 <qiuyang@live.unc.edu>
2020-06-25 10:27:15 -0400

    Add Lab 3

---
+++ lab3/Flik/testFilkandHorrible.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class testFilkandHorrible {
+    @Test
+    public void testFilk(){
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+
+
+}
