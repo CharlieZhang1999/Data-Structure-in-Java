0db13cadd8336a6a9613074ef5fd3607d672cda3
CharlieZhang1999 <qiuyang@live.unc.edu>
2020-07-09 22:13:23 -0400

    a

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,23 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t){
+        int x = p.x;
+        int y = p.y;
+
+    }
+
+    public static void addHexagon(TETile[][] world, int x, int y, int s, TETile t){
+        int longest = s + (s - 1) * 2;
+
+    }
+    public class Position{
+        public int x;
+        public int y;
+        public Position(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
 }
